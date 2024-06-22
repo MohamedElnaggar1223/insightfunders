81a87b44815e7e6b805ac4edc80f58e7db352405
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-06-23 00:59:13 +0300

    Mobile

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -9,7 +9,7 @@ export default function BottomBar()
     const pathname = usePathname()
 
     return (
-        <section className='px-4 py-3 bg-strong-gray flex items-center justify-around fixed w-full bottom-0 lg:hidden'>
+        <section className='px-4 py-3 bg-strong-gray flex items-center justify-around fixed w-full bottom-0 z-[99999] lg:hidden'>
             <Link href='/' className={cn('flex flex-col px-1.5 py-3.5 md:p-1.5 rounded-[10px] items-center justify-center gap-0.5 w-16 sm:w-20', pathname === '/' && 'bg-main-purple')}>
                 <Home size={24} stroke='#fff' />
                 <p className='text-white font-semibold text-xs max-sm:hidden'>Home</p>
