e815370245091000d02446f10fbc5a2510e241df
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2024-09-18 21:23:55 +0300

    Header

--- components/shared/HeaderButtons.tsx
+++ components/shared/HeaderButtons.tsx
@@ -13,7 +13,7 @@ export default async function HeadersButtons()
 			if(!user?.userStartUp?.EIN || !user?.userStartUp?.industry_sector || !user?.userStartUp.address || !user?.userStartUp.business_structure || !user?.userStartUp.company_name || !user?.userStartUp.email || !user?.userStartUp.phone_number || !user?.userStartUp.submitted) {
 				return (
                     <Link href='/startup-details' className='flex items-center gap-8 max-lg:w-full max-lg:flex-col max-lg:gap-4'>
-                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-sm h-9 max-lg:w-[338px]'>Continue</button>
+                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-base h-9 max-lg:w-[338px]'>Continue</button>
                         <SignOutButton />
                     </Link>
                 )
@@ -22,7 +22,7 @@ export default async function HeadersButtons()
 			if(user.userStartUpOwners?.length === 0) {
 				return (
                     <Link href='/startup-details' className='flex items-center gap-8 max-lg:w-full max-lg:flex-col max-lg:gap-4'>
-                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-sm h-9 max-lg:w-[338px]'>Continue</button>
+                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-base h-9 max-lg:w-[338px]'>Continue</button>
                         <SignOutButton />
                     </Link>
                 )
@@ -32,7 +32,7 @@ export default async function HeadersButtons()
             if(!user?.userInvestor?.submitted || !user?.userInvestor.company_email || !user?.userInvestor.company_name || !user?.userInvestor.company_email || !user?.userInvestor.company_website || !user?.userInvestor.geographies_served || !user?.userInvestor.max_facility_size || !user?.userInvestor.minimum_revenue_requirement || !user?.userInvestor.products_offered) {
                 return (
                     <Link href='/investor-details' className='flex items-center gap-8 max-lg:w-full max-lg:flex-col max-lg:gap-4'>
-                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-sm h-9 max-lg:w-[338px]'>Continue</button>
+                        <button className='bg-white rounded-[2px] w-32 font-light text-black text-base h-9 max-lg:w-[338px]'>Continue</button>
                         <SignOutButton />
                     </Link>
                 )
@@ -42,9 +42,9 @@ export default async function HeadersButtons()
 
     return (
         <div className='gap-8 flex items-center max-lg:w-full max-lg:flex-col max-lg:gap-4'>
-            <Link className='font-light text-sm' href='/sign-in'>Login</Link>
+            <Link className='font-light text-base' href='/sign-in'>Login</Link>
             <Link href='/sign-up'>
-                <button className='bg-white rounded-[2px] w-32 font-light text-black text-sm h-9 max-lg:w-[338px]'>Create account</button>
+                <button className='bg-white rounded-[2px] w-32 font-light text-black text-base h-9 max-lg:w-[338px]'>Create account</button>
             </Link>
         </div>
     )
