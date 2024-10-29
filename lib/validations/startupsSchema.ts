581838dce33cf8d766454e3464860d1466f551fb
vueexcel <vue.excel2011@gmail.com>
2024-10-29 16:01:02 +0530

    Fix select all file types

--- lib/validations/startupsSchema.ts
+++ lib/validations/startupsSchema.ts
@@ -1,49 +1,55 @@
 import * as z from 'zod'
 
 const MAX_FILE_SIZE = 5000000; // 5MB
-const ACCEPTED_FILE_TYPES = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+// const ACCEPTED_FILE_TYPES = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
 
 export const capTableSchema = z.object({
     name: z.string().min(1),
     document: z.instanceof(File).refine((value) => value.size <= MAX_FILE_SIZE, {
-        message: "Max file size is 5MB."
-    }).refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
-        message: "Only .pdf formats are supported."
+        message: "Max file size is 5MB.",
     })
+    // .refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
+    //     message: "Only .pdf formats are supported."
+    // })
 })
+
 
 export const pitchDeckSchema = z.object({
     name: z.string().min(1),
     document: z.instanceof(File).refine((value) => value.size <= MAX_FILE_SIZE, {
         message: "Max file size is 5MB."
-    }).refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
-        message: "Only .pdf formats are supported."
     })
+    // .refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
+    //     message: "Only .pdf formats are supported."
+    // })
 })
 
 export const taxReturnsSchema = z.object({
     name: z.string().min(1),
     document: z.instanceof(File).refine((value) => value.size <= MAX_FILE_SIZE, {
         message: "Max file size is 5MB."
-    }).refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
-        message: "Only .pdf formats are supported."
     })
+    // .refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
+    //     message: "Only .pdf formats are supported."
+    // })
 })
 
 export const financialStatementsSchema = z.object({
     name: z.string().min(1),
     document: z.instanceof(File).refine((value) => value.size <= MAX_FILE_SIZE, {
         message: "Max file size is 5MB."
-    }).refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
-        message: "Only .pdf formats are supported."
     })
+    // .refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
+    //     message: "Only .pdf formats are supported."
+    // })
 })
 
 export const legalDocumentsSchema = z.object({
     name: z.string().min(1),
     document: z.instanceof(File).refine((value) => value.size <= MAX_FILE_SIZE, {
         message: "Max file size is 5MB."
-    }).refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
-        message: "Only .pdf formats are supported."
     })
+    // .refine((value) => ACCEPTED_FILE_TYPES.includes(value.type), {
+    //     message: "Only .pdf formats are supported."
+    // })
 })
