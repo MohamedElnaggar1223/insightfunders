655fd4fbf63b630fa6db078c6e80ec455de0ddc6
Suhga Al Amin <developersuhag@gmail.com>
2024-11-27 22:09:01 +0600

    fixed bugs

--- components/startup/StartupSideBarLinks.tsx
+++ components/startup/StartupSideBarLinks.tsx
@@ -1,31 +1,59 @@
-'use client'
+"use client";
 
-import { cn } from "@/lib/utils"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import localFont from '@next/font/local';
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import localFont from "next/font/local";
 import { useEffect, useState } from "react";
 
+export default function StartupSideBarLinks() {
+  const pathname = usePathname();
+  const [data, setData] = useState("tejinder");
 
-export default function StartupSideBarLinks() 
-{
-    const pathname = usePathname()
-    const[data, setData] = useState("tejinder")
-    
-    useEffect(()=>{
-        const storedData = localStorage.getItem('hamburgerState');
-        if(storedData){
-            setData(JSON.parse(storedData))
-            console.log("data" , data)
-        }
-    },[])
-    return (
-        
-        <div className={`flex flex-col w-[90%] mt-8 gap-2 sidebardashboardMenuLinks`}>
-            <Link href='/' className={cn('py-2 text-sm t w-full', (!pathname.startsWith('/offers') && !pathname.startsWith('/manage')) ? 'bg-white font-medium text-black' : 'text-white')}>Dashboard</Link>
-            <Link href='/manage' className={cn('py-2 text-sm  w-full', pathname.startsWith('/manage') ? 'bg-white font-medium text-black' : 'text-white')}>Documents</Link>
-            <Link href='/offers' className={cn('py-2 text-sm  w-full', pathname.startsWith('/offers') ? 'bg-white font-medium text-black' : 'text-white')}>Offers</Link>
-          
-        </div>
-    )
+  useEffect(() => {
+    const storedData = localStorage.getItem("hamburgerState");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+      console.log("data", data);
+    }
+  }, []);
+  return (
+    <div
+      className={`flex flex-col w-[90%] mt-8 gap-2 sidebardashboardMenuLinks`}
+    >
+      <Link
+        href="/"
+        className={cn(
+          "py-2 text-sm t w-full",
+          !pathname.startsWith("/offers") && !pathname.startsWith("/manage")
+            ? "bg-white font-medium text-black"
+            : "text-white"
+        )}
+      >
+        Dashboard
+      </Link>
+      <Link
+        href="/manage"
+        className={cn(
+          "py-2 text-sm  w-full",
+          pathname.startsWith("/manage")
+            ? "bg-white font-medium text-black"
+            : "text-white"
+        )}
+      >
+        Documents
+      </Link>
+      <Link
+        href="/offers"
+        className={cn(
+          "py-2 text-sm  w-full",
+          pathname.startsWith("/offers")
+            ? "bg-white font-medium text-black"
+            : "text-white"
+        )}
+      >
+        Offers
+      </Link>
+    </div>
+  );
 }
