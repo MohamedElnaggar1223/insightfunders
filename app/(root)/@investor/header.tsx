6bb5286269328754141c2f2d5a75f58c38538032
KaushikTejani <tejanikaushik1999@gmail.com>
2024-12-06 15:47:05 +0530

    fix: redesign header profile banner

--- app/(root)/@investor/header.tsx
+++ app/(root)/@investor/header.tsx
@@ -10,7 +10,7 @@ export default async function HeaderInvestor() {
 
   return (
     <div className="ml-auto flex sticky items-center top-0 z-30 justify-end h-fit px-12 py-6 bg-[#212121] text-white w-full gap-3 shadow-lg navbarmobilemenu">
-      <div className="flex gap-3">
+      <div className="flex gap-3 items-center">
         <Link
           href="/profile"
           className="flex items-center justify-center gap-3"
@@ -18,12 +18,17 @@ export default async function HeaderInvestor() {
           <Avatar className="bg-[#F1F5F9] text-black border border-custom-gray">
             <AvatarImage src="" alt="company" />
             <AvatarFallback className="">
-              {user?.userStartUp?.company_name?.slice(0, 1)}
+              {user?.userInvestor?.company_name?.slice(0, 1)}
             </AvatarFallback>
           </Avatar>
-          <p className="text-white font-light text-sm userName">
-            {user?.userStartUp?.company_name}
-          </p>
+          <div className="flex flex-col justify-center">
+            <p className="text-[#FEFFFE] font-light text-sm leading-[17px]">
+              {user?.userInvestor?.company_name}
+            </p>
+            <p className="text-[#FEFFFE] font-light text-xs leading-[17px] self-start">
+              {user?.userInfo?.role}
+            </p>
+          </div>
         </Link>
       </div>
     </div>
